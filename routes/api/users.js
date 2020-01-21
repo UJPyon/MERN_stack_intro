3152d52118728ee3a1c07ffb062fdf9800fefcd4
Jae <ujpyon@gmail.com>
2020-01-21 14:45:20 -0500

    Revise login route to handle bcrypt password storage and comparison

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 const express = require("express");
 const router = express.Router();
 const validateRegisterInput = require("../../validation/register");
@@ -8,7 +10,7 @@ const User = require("../../models/User");
 // --GET Route for Users--
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
-// -- POST Route for Users--
+// -- POST Route for creating new Users--
 router.post("/register", (req, res) => {
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email }).then(user => {
@@ -36,6 +38,38 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// --POST route for logging in existing User--
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "This user does not exist" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id: user.id, handle: user.handle };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          // Tell the key to expire in one hour
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Incorrect password" });
+      }
+    });
   });
 });
 
