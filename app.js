ab61152b558315ae51ae981a517d786816addb84
Jae <ujpyon@gmail.com>
2020-01-21 14:16:03 -0500

    Add chrome dev tools script

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
 
 mongoose
   .connect(db, { 
@@ -14,7 +15,13 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  console.log(res);
+  debugger
+  res.send("Hello World");
+});
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.listen(port, () => console.log(`Server is running on ${port}`));
